0388112d7c68431e51c9788efa9cb6dca42e58bb
Gsoft Dev <dev.gsoft@gmail.com>
2020-05-11 20:13:04 +0300

    Getting user input

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,23 +2,17 @@
 //const fs = require('fs');
 
 //import npm modules
-const validator = require("validator");
-//import validator from "validator"; //es6
 const chalk = require("chalk");
 
 //import project files
-//const utils = require('./utils');
 const notes = require('./notes');
-//import notes from "./notes";
 
-//get the notes
-var notesList = notes.getNotes();
-console.log(notesList);
-
-//check if email is valid
-console.log(validator.isEmail("dev.gsoft@gmail.com"));
-console.log(validator.isURL("https://wakikevin.io"));
-
-//chalk challenge
-console.log(chalk.green("Success!"));
-console.log(chalk.red.bold.inverse(`This is an error.`));
+//decalre variables
+var command = process.argv[2];
+console.log(process.argv)
+//check what command to run
+if(command === "add"){
+    console.log(`adding note`);
+}else if(command === "remove"){
+    console.log(`removing note`);
+}
