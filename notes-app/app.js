c2ce6f55ad63099977818007e1e0d6d87ff54671
Gsoft Dev <dev.gsoft@gmail.com>
2020-05-11 14:31:14 +0300

    importing npm modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,23 +1,17 @@
 //import the fs module
 //const fs = require('fs');
 
+//import npm modules
+const validator = require("validator");
+
 //import project files
 //const utils = require('./utils');
 const notes = require('./notes');
 
-//write to file
-//fs.writeFileSync('notes.txt', "This is my first file by Node.js");
-
-//append data to file
-//fs.appendFileSync('notes.txt', "This is appended text for the challenge!");
-
-//define variable
-//const name = "Kevin";
-//const sum = utils.add(1,1);
-//console.log(sum);
-
-//console.log(utils.name);
-
 //get the notes
 var notesList = notes.getNotes();
 console.log(notesList);
+
+//check if email is valid
+console.log(validator.isEmail("dev.gsoft@gmail.com"));
+console.log(validator.isURL("https://wakikevin.io"));
