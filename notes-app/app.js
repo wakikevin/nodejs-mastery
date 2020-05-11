e5bd23e81f2df1fcf9d399520bbf0e96e5a63650
Gsoft Dev <dev.gsoft@gmail.com>
2020-05-11 20:29:43 +0300

    Getting command line arguments using YARGS

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,16 +3,54 @@
 
 //import npm modules
 const chalk = require("chalk");
+const yargs = require("yargs");
 
 //import project files
 const notes = require('./notes');
 
-//decalre variables
-var command = process.argv[2];
-console.log(process.argv)
-//check what command to run
-if(command === "add"){
-    console.log(`adding note`);
-}else if(command === "remove"){
-    console.log(`removing note`);
-}
+//delare variables
+//var command = process.argv[2];
+
+//customize yargs varsion
+yargs.version('1.1.0');
+
+
+//command required for notes app add, remove, read, list
+//setup the add command
+yargs.command({
+    command:'add',
+    describe: "Add a new note",
+    handler: () =>{
+        console.log("adding a new note!");
+    }
+});
+
+//setup the add command
+yargs.command({
+    command:'remove',
+    describe: "Remove a new note",
+    handler: () =>{
+        console.log("removing a note!");
+    }
+});
+
+//setup the read command
+yargs.command({
+    command:'read',
+    describe: "Read a new note",
+    handler: () =>{
+        console.log("reading a note!");
+    }
+});
+
+//setup the list command
+yargs.command({
+    command:'list',
+    describe: "List all notes",
+    handler: () =>{
+        console.log("listing all notes!");
+    }
+});
+
+
+console.log(yargs.argv);
