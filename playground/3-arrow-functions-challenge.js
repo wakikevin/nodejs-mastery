e0d624bfeafad1a6178531092318450e9de09ee8
Gsoft Dev <dev.gsoft@gmail.com>
2020-05-12 09:40:04 +0300

    update to better way of returning tasks

--- playground/3-arrow-functions-challenge.js
+++ playground/3-arrow-functions-challenge.js
@@ -17,7 +17,8 @@ const tasks = {
         completed: false
     }],
     getTasksToDo(){
-        return this.tasks.filter((task) => task.completed === false);
+        //return this.tasks.filter((task) => task.completed === false);
+        return this.tasks.filter((task) => !task.completed);
     }
 }
 console.log(tasks.getTasksToDo())
